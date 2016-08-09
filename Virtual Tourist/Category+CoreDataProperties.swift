1b207bc544c0c04271007bd24a1ab747a6d1f5d1
Johnny Parham <johnny_parham@hotmail.com>
2016-08-09 10:26:25 -0400

    Added Category class

---
+++ Virtual Tourist/Category+CoreDataProperties.swift
@@ -0,0 +1,15 @@
+//
+//  Category+CoreDataProperties.swift
+//  Virtual Tourist
+//
+//  Created by Johnny Parham on 8/9/16.
+//  Copyright © 2016 Johnny Parham. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Category {
+    
+    @NSManaged var categoryName: String?
+}
---
+++ Virtual Tourist/Category.swift
@@ -0,0 +1,14 @@
+//
+//  Category.swift
+//  Virtual Tourist
+//
+//  Created by Johnny Parham on 8/9/16.
+//  Copyright © 2016 Johnny Parham. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Category: NSManagedObject {
+
+}
